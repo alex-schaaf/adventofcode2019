edddcd91c28d5c59431d9f64b94f605fb31e81b8
pytzcarraldo <a.schaaf@abdn.ac.uk>
2019-12-17 16:59:08 +0100

    01

--- 01.py
+++ 01.py
@@ -1,12 +1,22 @@
 import numpy as np
-from advent import fuel_required
+from advent import mass_fuel_requirements, mass_fuel_recursive
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         sum_fuel = [
-            fuel_required(
+            mass_fuel_requirements(
                 int(mass.rstrip())
             ) for mass in f.readlines()
         ]
-    print(int(sum(sum_fuel)))
+
+    with open("input.txt", "r") as f: 
+        sum_fuel_recursive = [
+            mass_fuel_recursive(
+                int(mass.rstrip())
+            ) for mass in f.readlines()
+        ]
+
+    print(
+        f"{int(sum(sum_fuel))}, recursive: {int(sum(sum_fuel_recursive))}"
+    )
--- advent/__init__.py
+++ advent/__init__.py
@@ -1,5 +1,19 @@
 import numpy as np
+from copy import copy
 
 
-def fuel_required(mass:float) -> float:
-    return np.floor(mass / 3) - 2
+def mass_fuel_requirements(mass:float) -> float:
+    fuel = np.floor(mass / 3) - 2
+    if fuel < 0:
+        fuel = 0
+    return fuel
+
+
+def mass_fuel_recursive(mass:float):
+    fuel = mass_fuel_requirements(mass)
+    additional_fuel = copy(fuel)
+    while additional_fuel > 0:
+        additional_fuel = mass_fuel_requirements(additional_fuel)
+        fuel += additional_fuel
+    return fuel
+

8da5b5e9916ab45938f726971a9791afed76628d
Alex <pytzcarraldo@gmail.com>
2019-12-23 00:51:19 +0100

    05_2 solved

--- 05/05.py
+++ 05/05.py
@@ -4,23 +4,6 @@ with open("05/input.txt", "r") as f:
     program = [int(i) for i in f.read().split(",")]
 
 
-def add(program:list, loc1:int, loc2:int) -> int:
-    return program[loc1] + program[loc2]
-
-
-def multiply(program:list, loc1:int, loc2:int) -> int:
-    return program[loc1] * program[loc2]
-
-
-def input_at(program:list, loc1:int) -> int:
-    simulated_input = 1  # alternative to user input
-    return simulated_input
-
-
-def output_from(program:list, loc1:int) -> int:
-    return program[loc1]
-
-
 def parse_program(program:list) -> list:
     pointer = 0
     while True:        
@@ -33,17 +16,39 @@ def parse_program(program:list) -> list:
         loc3 = program[pointer + 3]
 
         if opcode == 1:
-            program[loc3] = add(program, loc1, loc2)
+            program[loc3] = program[loc1] + program[loc2]
             pointer += 4
         elif opcode == 2:
-            program[loc3] = multiply(program, loc1, loc2)
+            program[loc3] = program[loc1] * program[loc2]
             pointer += 4
         elif opcode == 3:
-            program[program[pointer + 1]] = 1
+            program[program[pointer + 1]] = 5
             pointer += 2
         elif opcode == 4:
-            print(output_from(program, loc1))
+            print(program[loc1])
             pointer += 2
+        elif opcode == 5:
+            if program[loc1] != 0:
+                pointer = program[loc2]
+            else:
+                pointer += 3
+        elif opcode == 6:
+            if program[loc1] == 0:
+                pointer = program[loc2]
+            else:
+                pointer += 3
+        elif opcode == 7:
+            if program[loc1] < program[loc2]:
+                program[loc3] = 1
+            else:
+                program[loc3] = 0
+            pointer += 4
+        elif opcode == 8:
+            if program[loc1] == program[loc2]:
+                program[loc3] = 1
+            else:
+                program[loc3] = 0
+            pointer += 4
         elif opcode == 99:
             break
     
